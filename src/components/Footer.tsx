b35601d7e4ddf1b7764cf94739c9c860c5db32b7
dmchoi <david5760@naver.com>
2022-01-22 14:39:30 +0900

    Update - SNS 링크 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,15 @@ import React from 'react'
 const Footer: React.FC = () => {
     return (
         <div className="footer">
-            <img src="img/twitter.png" alt="twitter"></img>
-            <img src="img/facebook.png" alt="facebook"></img>
-            <img src="img/instagram.png" alt="instagram"></img>
+            <a href="https://twitter.com/_iuofficial" target="_blank" rel="noreferrer">
+                <img src="img/twitter.png" alt="twitter" />
+            </a>
+            <a href="https://ko-kr.facebook.com/iu.loen" target="_blank" rel="noreferrer">
+                <img src="img/facebook.png" alt="facebook"></img>
+            </a>
+            <a href="https://www.instagram.com/dlwlrma/" target="_blank" rel="noreferrer">
+                <img src="img/instagram.png" alt="instagram"></img>
+            </a>
         </div>
     )
 }
